07353c3461392d45eb2f209e349be696c33a2a55
diasvictordev <victor.dias@aluno.ueg.br>
2023-08-21 16:29:34 -0300

    commit inicial

---
+++ src/main/java/com/challenge3/challenge3/compass/Challenge3CompassApplication.java
@@ -0,0 +1,13 @@
+package com.challenge3.challenge3.compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge3CompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge3CompassApplication.class, args);
+	}
+
+}
